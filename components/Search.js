cb125b5c6141663762d661b48414723b40c0dd2c
Colin Hunwald <colin.hunwald@gmail.com>
2022-04-24 16:28:59 +0200

    Add search icon

--- components/Search.js
+++ components/Search.js
@@ -1,11 +1,25 @@
+import Image from 'next/image';
+
 const Search = () => {
   return (
     <div>
-      <input
-        className="input is-primary"
-        type="text"
-        placeholder="Search for Products"
-      ></input>
+      <p className="control has-icons-left">
+        <input
+          className="input is-primary"
+          type="text"
+          placeholder="Search for Products"
+        ></input>
+        <span className="icon is-small is-left">
+          <figure class="image is-24x24">
+            <Image
+              width={24}
+              height={24}
+              src="/search.png"
+              alt="Lifing accessories"
+            />
+          </figure>
+        </span>
+      </p>
     </div>
   );
 };
