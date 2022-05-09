09ca2ae5275187f60d1d2a8f1f01f467c6a9b958
Colin Hunwald <colin.hunwald@gmail.com>
2022-05-09 18:40:59 +0200

    Fix SearchItems and delete Searchitem.js

--- pages/SearchItems/index.js
+++ pages/SearchItems/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { createClient } from 'contentful';
-import SearchItem from './SearchItem';
+import Image from 'next/image';
 
 export async function getServerSideProps(context) {
   const client = createClient({
@@ -22,22 +22,69 @@ export async function getServerSideProps(context) {
 
 const SearchTerms = (props) => {
   const router = useRouter(props);
-  console.log('Path: ' + router.pathname);
-  console.log('ser:' + props.searchItems);
-  console.log(
-    'searchy: ' + props.searchItems[0].fields.references[0].fields.name
-  );
+
   const [term, setTerm] = useState('');
 
   useEffect(() => {
     setTerm(router.query.name);
   }, [router.query.name]);
 
+  let refArray = [];
+  let resFiltered = [];
+
+  props.searchItems.length >= 1 && term
+    ? (resFiltered = props.searchItems.filter(
+        (item) => item.fields.search === term.toLowerCase()
+      ))
+    : console.log('No');
+
+  resFiltered.length >= 1
+    ? (refArray = resFiltered[0].fields.references)
+    : console.log('No');
+
   return term !== '' ? (
-    <div>
-      <SearchItem term={term} searchItems={props.searchItems} />
-    </div>
+    <section className="section mt-6">
+      <article className="panel is-primary">
+        <p className="panel-heading">
+          <span>Search hits for: </span>
+          <span className="has-text-warning is-size-5">...{term}...</span>
+        </p>
+
+        {refArray.length >= 1 ? (
+          refArray.map((ref) => {
+            return (
+              <a key={ref.fields.name} className="panel-block is-active">
+                <span>
+                  <Image
+                    width={40}
+                    height={40}
+                    src="/searchIcon.png"
+                    alt="Search Icon"
+                  />
+                </span>
+
+                {ref.fields.productGroupId}
+                <span className="panel-icon">
+                  <i className="fas fa-book" aria-hidden="true"></i>
+                </span>
+                {ref.fields.name}
+              </a>
+            );
+          })
+        ) : (
+          <a className="panel-block is-active">
+            <span className="panel-icon">
+              <i className="fas fa-book" aria-hidden="true"></i>
+            </span>
+            no data
+          </a>
+        )}
+      </article>
+    </section>
   ) : (
+    // <div>
+    //   {/* <SearchItem term={term} searchItems={props.searchItems} /> */}
+    // </div>
     <div></div>
   );
 };
